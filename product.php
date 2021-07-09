a837435bfdad70fadca77d76d2a5777f58bbb3a1
arul-25 <arulprojectdualima@gmail.com>
2021-07-09 20:57:46 +0900

    membuat file product.php dan _products.php

---
+++ product.php
@@ -0,0 +1,13 @@
+<?php
+
+/* Header */
+include("header.php");
+
+/* Top Sale */
+include("Template/_top-sale.php");
+
+/* Products */
+include("Template/_products.php");
+
+/* Footer */
+include("footer.php");
