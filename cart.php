42cdb342c4bb4ceed8fc24bfb9e2cf1123d31b34
arul-25 <arulprojectdualima@gmail.com>
2021-07-14 15:35:53 +0900

    require autoload dan function di file cart.php

--- cart.php
+++ cart.php
@@ -1,5 +1,11 @@
 <?php
 
+// require autoload composer
+require_once __DIR__ . "/vendor/autoload.php";
+
+// require Functio 
+require __DIR__ . "/function.php";
+
 /* Header */
 include("header.php");
 
